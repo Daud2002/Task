2f763e0d267a7481a180b254e2e29629583e1f34
daudm <daudm@DAUDMIR>
2022-11-29 22:45:19 +0500

    Add project files.

---
+++ Projects/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Projects.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
